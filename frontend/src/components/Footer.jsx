f1e16ef0a28e6564d1a7dc6af9d9845cefad13db
khajaameen455 <carloidonme@gmail.com>
2024-06-04 13:10:42 +0530

    Email fuctionality

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        Designed And Developed By Khaja_ameen
+    </footer>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <p>SULTAN FITNESS CLUB</p>
+    </header>
+  );
+};
+
+export default Navbar;
